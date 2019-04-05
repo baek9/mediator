65adb7a099017fc80278ff3a0e0bacdf701bca17
Taeho Nam <thn7440@gmail.com>
2019-04-06 03:50:18 +0900

    update working autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -1,20 +1,24 @@
 DATE=$(date +%Y-%m-%d)
+
 # clone posts
 git clone $1
 
-# jekyll build
-bundle exec jekyll build -d ../site
+# clone sites & settings
+git clone https://github.com/baek9/baek9.github.io.git ../site
 cd ../site
-pwd
+git config user.name "Taeho Nam"
+git config user.email "thn7440@gmail.com"
+rm -rf ./*
+
+# jekyll build
+cd ../mediator
+bundle exec jekyll build -d ../site
 
 # autopush
-git init
-git remote add origin https://github.com/baek9/baek9.github.io.git
+cd ../site
 rm README.md
 rm autopush.sh
 git add .
-git config user.name "Taeho Nam"
-git config user.email "thn7440@gmail.com"
 git commit -m "$DATE"
 git log
 git push -u origin master
